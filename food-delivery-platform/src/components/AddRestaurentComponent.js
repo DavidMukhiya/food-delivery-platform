67945bb5156e1ccae1e97e45d5df2c7fae10e839
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-10-21 22:55:11 -0700

    add restaurant page is complete without file adding field

--- food-delivery-platform/src/components/AddRestaurentComponent.js
+++ food-delivery-platform/src/components/AddRestaurentComponent.js
@@ -5,7 +5,7 @@ import "../css/addrestaurantstyle.css";
 const AddRestaurentComponent = () => {
   return (
     <div className="addRestaurantComponentStyle">
-      <p style={{ textTransform: "uppercase", color: "#5B4F47" }}>
+      <p style={{ textTransform: "uppercase", color: "#5B4F47", fontSize: '1.5rem' }} className='mt-5'>
         Add Restaurant
       </p>
       <Form>
@@ -22,7 +22,7 @@ const AddRestaurentComponent = () => {
             name="restaurantName"
             placeholder="Restaurant Name *"
             type="text"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem" }}
+            style={{ backgroundColor: "#DDC1A7", width: "25rem", margin: "1.5rem 0rem" }}
           />
         </FormGroup>
         <FormGroup>
@@ -39,7 +39,7 @@ const AddRestaurentComponent = () => {
             placeholder="Email Address *"
             type="email"
             className="addRestaurantInputStyle"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem" }}
+            style={{ backgroundColor: "#DDC1A7", width: "25rem", marginBottom:'1.5rem'  }}
           />
         </FormGroup>
         <FormGroup>
@@ -56,10 +56,10 @@ const AddRestaurentComponent = () => {
             placeholder="Set A Password*"
             type="password"
             className="addRestaurantInputStyle"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem" }}
+            style={{ backgroundColor: "#DDC1A7", width: "25rem", margin:'1.5rem 0' }}
           />
         </FormGroup>
-        <Button style={{ backgroundColor: "#BD9479", color: "#5B4F47", display: 'block',}} className='mx-auto'>
+        <Button style={{ backgroundColor: "#BD9479", color: "#5B4F47", display: 'block', marginTop:'2.5rem'}} className='mx-auto'>
           Sign Up
         </Button>
       </Form>
