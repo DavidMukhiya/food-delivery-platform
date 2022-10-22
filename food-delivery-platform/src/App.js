d988b54e40fbbe693350c7c02228811c43c37d67
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-10-22 00:01:00 -0700

    added add dish component to add dishes

--- food-delivery-platform/src/App.js
+++ food-delivery-platform/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import AddDishComponent from "./components/AddDishComponent";
 import AddRestaurentComponent from "./components/AddRestaurentComponent";
 import YourRestaurantComponent from "./components/YourRestaurantComponent";
 
@@ -6,6 +7,7 @@ function App() {
   return <div className="App">
   <AddRestaurentComponent/>
   <YourRestaurantComponent/>
+  <AddDishComponent/>
   </div>;
   
 }
--- food-delivery-platform/src/components/YourRestaurantComponent.js
+++ food-delivery-platform/src/components/YourRestaurantComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import "../css/addrestaurantstyle.css";
 
 const YourRestaurantComponent = () => {
   return (
