c583bcaa49c2c36d60105783239240f9e5f6172a
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-11-19 18:14:24 -0800

    complete handing the form for add restaurant except for file

--- food-delivery-platform/src/components/AddRestaurentComponent.js
+++ food-delivery-platform/src/components/AddRestaurentComponent.js
@@ -4,41 +4,56 @@ import { Form, FormGroup, Input, Label, Button } from "reactstrap";
 import "../css/addrestaurantstyle.css";
 
 const AddRestaurentComponent = () => {
+  const [data, setData] = useState({
+    restaurantName: "",
+    emailAddress: "",
+    password: "",
+  });
 
-  const [data, setData]=useState({
-    restaurantName:'',
-    emailAddress:'',
-    password:'',
-    file:''
-  })
+  const [file, setfile] = useState();
 
   const [error, setError] = useState({
-    errors:{},
-    isError:false
-  })
+    errors: {},
+    isError: false,
+  });
 
-  useEffect(()=>{
-    console.log(data)
-  },[data])
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
-  const handleChange=(event)=>{
-    setData({
-      ...data, restaurantName: event.target.value
-      
-    })
+  const handleChange = (event, property) => {
+    setData({ ...data, [property]: event.target.value });
+    console.log(data);
+  };
+
+  const handleFileChange = (event) => {
+    console.log(event.target.files);
+    //setfile(event.taget.files);
+  };
+
+  const submitForm=(event)=>{
+    event.preventDefault()
     console.log(data)
+    
+    //data validate
+
+    //call server api for sending data
   }
 
   return (
     <div className="addRestaurantComponentStyle">
-      <p style={{ textTransform: "uppercase", color: "#5B4F47", fontSize: '1.5rem' }} className='mt-5'>
+      <p
+        style={{
+          textTransform: "uppercase",
+          color: "#5B4F47",
+          fontSize: "1.5rem",
+        }}
+        className="mt-5"
+      >
         Add Restaurant
       </p>
-
-      
-      <Form>
-
-      {/* Restaurant Name */}
+      <Form onSubmit={submitForm} >
+        {/* Restaurant Name */}
         <FormGroup>
           <Label
             for="restaurantName"
@@ -52,8 +67,14 @@ const AddRestaurentComponent = () => {
             name="restaurantName"
             placeholder="Restaurant Name *"
             type="text"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem", margin: "1.5rem 0rem" , textAlign:'center'  }}
-            onChange={(e)=>handleChange(e)}
+            style={{
+              backgroundColor: "#DDC1A7",
+              width: "25rem",
+              margin: "1.5rem 0rem",
+              textAlign: "center",
+            }}
+            onChange={(e) => handleChange(e, "restaurantName")}
+            value={data.restaurantName}
           />
         </FormGroup>
 
@@ -72,10 +93,16 @@ const AddRestaurentComponent = () => {
             placeholder="Email Address *"
             type="email"
             className="addRestaurantInputStyle"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem", marginBottom:'1.5rem', textAlign:'center'   }}
+            onChange={(e) => handleChange(e, "emailAddress")}
+            value={data.emailAddress}
+            style={{
+              backgroundColor: "#DDC1A7",
+              width: "25rem",
+              marginBottom: "1.5rem",
+              textAlign: "center",
+            }}
           />
         </FormGroup>
-
 
         {/* passsword */}
         <FormGroup>
@@ -91,24 +118,46 @@ const AddRestaurentComponent = () => {
             name="password"
             placeholder="Set A Password*"
             type="password"
+            onChange={(e) => handleChange(e, "password")}
             className="addRestaurantInputStyle"
-            style={{ backgroundColor: "#DDC1A7", width: "25rem", margin:'1.5rem 0', textAlign:'center' }}
+            style={{
+              backgroundColor: "#DDC1A7",
+              width: "25rem",
+              margin: "1.5rem 0",
+              textAlign: "center",
+            }}
+            value={data.password}
           />
         </FormGroup>
 
         {/* file */}
         <FormGroup>
-                <Label for="file" className="visually-hidden">Image</Label>
-                <Input id="file" name="file" type="file"
-                style={{
-                    backgroundColor: "#DDC1A7",
-                    width: "25rem",
-                    margin: "1.5rem 0",
-                    textAlign: "center",
-                    color: "#6C757D",
-                  }} />
-              </FormGroup>
-        <Button style={{ backgroundColor: "#BD9479", color: "#5B4F47", display: 'block', marginTop:'2.5rem'}} className='mx-auto'>
+          <Label for="file" className="visually-hidden">
+            Health Doc
+          </Label>
+          <Input
+            id="file"
+            name="file"
+            type="file"
+            onChange={(e) => handleFileChange(e)}
+            style={{
+              backgroundColor: "#DDC1A7",
+              width: "25rem",
+              margin: "1.5rem 0",
+              textAlign: "center",
+              color: "#6C757D",
+            }}
+          />
+        </FormGroup>
+        <Button
+          style={{
+            backgroundColor: "#BD9479",
+            color: "#5B4F47",
+            display: "block",
+            marginTop: "2.5rem",
+          }}
+          className="mx-auto"
+        >
           Sign Up
         </Button>
       </Form>
